aca01718da234427a1f243f5abe73712b2d803ce
Tobzsolo <oluwatobasolo@gmail.com>
2024-08-11 18:43:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch(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)
+.then(x => x.text())
+.then(y => myDisplay(y));
+async function Getrepos(https://api.github.com/repos/Tobzsol/REPO) {
+ let x = await fetch(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)
+ let y = await x.Repos();
+ myDisplay(y);
+}
